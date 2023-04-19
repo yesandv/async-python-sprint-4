fe3ededad7e12b9e2d1df50d8783c77e890c76cc
yesandv <grc@protonmail.com>
2023-04-19 19:56:56 +0300

    Add descriptions

--- src/api/db.py
+++ src/api/db.py
@@ -7,6 +7,9 @@ from src.services.short_url import url_crud
 db_router = APIRouter(prefix="/db", tags=["DB"])
 
 
-@db_router.get("/ping")
+@db_router.get(
+    "/ping",
+    description="Checks if the database connection is working."
+)
 async def ping(session: AsyncSession = Depends(get_session)) -> dict:
     return await url_crud.ping(session=session)
--- src/api/urls.py
+++ src/api/urls.py
@@ -1,18 +1,22 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.openapi.models import Response
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
 from starlette.responses import RedirectResponse
 
 from src.core.logging_config import logger
 from src.db.db import get_session
 from src.schemas.url import FullUrl, ShortUrl, UrlInDB, UrlUpdate
 from src.services.short_url import url_crud
-from src.utils.exceptions import GoneException
 
 url_router = APIRouter(prefix="/urls", tags=["URLs"])
 
 
-@url_router.post("/shorten", response_model=ShortUrl)
+@url_router.post(
+    "/shorten",
+    response_model=ShortUrl,
+    description="Creates a shortened URL. The response is a ShortUrl schema.",
+)
 async def shorten_url(
         *,
         schema: FullUrl,
@@ -22,14 +26,19 @@ async def shorten_url(
     return await url_crud.create(session=session, schema=schema)
 
 
-@url_router.get("/{url_id}", response_class=RedirectResponse)
+@url_router.get(
+    "/{url_id}",
+    response_class=RedirectResponse,
+    description="Redirects the user to the corresponding full URL.",
+)
 async def redirect_url(
         *,
         url_id: str,
         session: AsyncSession = Depends(get_session),
 ) -> RedirectResponse:
     _url = await url_crud.get(session=session, url_id=url_id)
-    GoneException(_url)
+    if _url.is_taken_down:
+        raise HTTPException(status_code=status.HTTP_410_GONE, detail="Gone")
     url = await url_crud.update(
         session=session, url_id=url_id, clicks=_url.clicks + 1
     )
@@ -37,22 +46,32 @@ async def redirect_url(
     return RedirectResponse(url=url.full_url)
 
 
-@url_router.get("/{url_id}/status", response_model=UrlInDB)
+@url_router.get(
+    "/{url_id}/status",
+    response_model=UrlInDB,
+    description="Retrieves the corresponding URL from the database.",
+)
 async def get_url_status(
         *, url_id: str, session: AsyncSession = Depends(get_session)
 ) -> Response:
     logger.info("Retrieving %s from the DB", url_id)
     url = await url_crud.get(session=session, url_id=url_id)
-    GoneException(url)
+    if url.is_taken_down:
+        raise HTTPException(status_code=status.HTTP_410_GONE, detail="Gone")
     return url
 
 
-@url_router.delete("/{url_id}", response_model=UrlUpdate)
+@url_router.delete(
+    "/{url_id}",
+    response_model=UrlUpdate,
+    description="Marks the corresponding URL as deleted in the database.",
+)
 async def delete_url(
         *, url_id: str, session: AsyncSession = Depends(get_session)
 ) -> Response:
     url = await url_crud.update(
         session=session, url_id=url_id, is_taken_down=True
     )
-    GoneException(url)
+    if url.is_taken_down:
+        raise HTTPException(status_code=status.HTTP_410_GONE, detail="Gone")
     return url
